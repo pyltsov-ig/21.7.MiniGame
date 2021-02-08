8ed3d5f1a69b59fb15179d43a1c73367ebc292ca
IGP <pyltsov@gmail.com>
2021-02-08 17:46:08 +0100

    with animation

--- MiniGameI/CircleCodeView.swift
+++ MiniGameI/CircleCodeView.swift
@@ -20,19 +20,30 @@ import UIKit
     }
     
     func setSize(radius:CGFloat) {
-        frame.size.width = radius
-        frame.size.height = radius
+       
+        self.frame.size.height = radius
+        self.frame.size.width = radius
         setCircle(radius: radius)
+       
         //layer.cornerRadius = frame.height / 2
     }
     
     func setCircle(radius:CGFloat) {
         layer.cornerRadius = radius / 2
+        //UIView.animate(withDuration: 3, animations: {self.layer.cornerRadius = radius / 2})
+//        UIView.animate(withDuration: 1.0, delay: 0, options: .curveEaseIn, animations: {self.layoutIfNeeded()}, completion: nil)
+        //animate cornerRadius and update model
+        
+        
+//        UIView.animate(withDuration: 3, delay: 0, options: .curveEaseIn, animations: {self.layer.cornerRadius = radius / 2}, completion: nil)
+//
     }
     
     func changeColor(){
        // layer.backgroundColor = CGColor(
-        layer.backgroundColor = CGColor(red: .random(in: 0...2), green: .random(in: 0...2), blue: .random(in: 0...2), alpha: 1.0)
+        let bgColor = CGColor(red: .random(in: 0...2), green: .random(in: 0...2), blue: .random(in: 0...2), alpha: 1.0)
+        UIView.animate(withDuration: 2, animations: {self.backgroundColor = UIColor(cgColor: bgColor)})
+        //layer.backgroundColor = CGColor(red: .random(in: 0...2), green: .random(in: 0...2), blue: .random(in: 0...2), alpha: 1.0)
     }
     
     override func prepareForInterfaceBuilder() {
